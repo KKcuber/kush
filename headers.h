3fb108755e8b55f61782614aab05e5614a3360ce
KKcuber <kkcubes@gmail.com>
2021-09-11 22:28:22 +0530

    Initial commit

---
+++ headers.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <stdlib.h>
+#include <grp.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+char cwd[100000];
+char sysname[100000];
+char username[1000000];
