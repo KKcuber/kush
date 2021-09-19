3f2c5b3f234def8ba1afa20047a7173bf252f0f5
KKcuber <kkcubes@gmail.com>
2021-09-19 18:27:48 +0530

    added 6 month functionality to ls

--- ls.c
+++ ls.c
@@ -204,12 +204,16 @@ void ls(int numTokens)
                 char *group = getgrgid(filedetails.st_gid)->gr_name;
 
                 int size = filedetails.st_size;
-                char *time = (char *)malloc(100 * sizeof(char));
-                strftime(time, 100, "%b %d %H:%M", localtime(&(filedetails.st_ctime)));
-
-                printf("%s\t%d\t%s\t%s\t%d\t%s\t%s\n", permissions, hardlink_number, owner, group, size, time, listOfFiles[i]->d_name);
+                char *filetime = (char *)malloc(100 * sizeof(char));
+                time_t currentTime = 0;
+                currentTime = time(NULL);
+                if(currentTime - filedetails.st_mtim.tv_sec < 15780000)
+                    strftime(filetime, 100, "%b %d %H:%M", localtime(&(filedetails.st_mtime)));
+                else
+                    strftime(filetime, 100, "%b %d %Y", localtime(&(filedetails.st_mtime)));
+                printf("%s\t%d\t%s\t%s\t%d\t%s\t%s\n", permissions, hardlink_number, owner, group, size, filetime, listOfFiles[i]->d_name);
                 free(filepath);
-                free(time);
+                free(filetime);
                 free(permissions);
             }
         }
